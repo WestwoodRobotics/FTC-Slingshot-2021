1822b93c352ba71905889eb6889ec33a44cab770
DrIronfist <reetsinha2006@gmail.com>
2021-10-22 23:38:39 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,3 +1,10 @@
-public class Teleop {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "test", group="Differential")
+public class Teleop extends OpMode {
     int bruh = 0;
 }
