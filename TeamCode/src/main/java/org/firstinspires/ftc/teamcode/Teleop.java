d73b9a123b0e6faa848bbfd5503101a83e50cae0
DrIronfist <reetsinha2006@gmail.com>
2021-10-22 23:52:30 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,9 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp(name = "test", group="Differential")
 public class Teleop extends OpMode {
     int bruh = 1;
+    ElapsedTime runtime = new ElapsedTime();
 }
