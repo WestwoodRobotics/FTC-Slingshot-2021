c2fd2a7482ff780c73dd61df9c749411d4083d2a
DrIronfist <reetsinha2006@gmail.com>
2021-10-23 22:15:41 -0500

    drivetrain test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -6,14 +6,63 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 
-@TeleOp(name = "test", group="Differential")
-public class Teleop extends OpMode {
-    ElapsedTime runtime = null;
+@TeleOp(name = "test", group="TeleOp")
+public class Teleop extends LinearOpMode {
+    ElapsedTime runtime = new ElapsedTime();
     DcMotor leftFront = null;
     DcMotor rightFront = null;
     DcMotor leftBack = null;
     DcMotor rightBack = null;
+    DcMotor cascadeMotor = null;
 
+    @Override
+    void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        leftFront  = hardwareMap.get(DcMotor.class, "left_Front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_Back");
+        rightFront  = hardwareMap.get(DcMotor.class, "right_Front");
+        rightBack = hardwareMap.get(DcMotor.class, "right_Back");
+        cascadeMotor = hardwareMap.get(DcMotor.class, "cascade");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        cascadeMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            //drivetrain
+            double leftPower = 0;
+            double rightPower = 0;
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            double mag = Math.magnitude(drive, turn);
+            if(mag < 1){
+                leftPower = Range.clip(drive + turn, -1.0, 1.0) ;
+                rightPower = Range.clip(drive - turn, -1.0, 1.0) ;
+            } else {
+                leftPower = Range.clip(drive / mag + turn / mag, -1.0, 1.0);
+                rightPower = Range.clip(drive / mag - turn / mag, -1.0, 1.0);
+            }
+            leftFront.setPower(leftPower);
+            rightFront.setPower(rightPower);
+            leftBack.setPower(leftPower);
+            rightBack.setPower(rightPower);
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+
+            //Cascade
+
+
+            telemetry.update();
+        }
+
+    }
 }
